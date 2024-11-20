7ea8ed39235bbc77e3232934883b646d90011300
NakulSiwach <nakulsiwach007@gmail.com>
2024-11-20 21:08:09 +0530

    basic structure

---
+++ src/main/java/com/nakulsiwach/esd_2/Esd2Application.java
@@ -0,0 +1,13 @@
+package com.nakulsiwach.esd_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Esd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nakulsiwach/esd_2/Esd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakulsiwach.esd_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
